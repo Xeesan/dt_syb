f6068d628bc219184cdb9985ce39b4785ba80030
Xisan <ziauddinzisan2001@gmail.com>
2021-11-05 11:01:10 +0600

    ded

--- syberia_beryllium.mk
+++ syberia_beryllium.mk
@@ -8,8 +8,6 @@ $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
 # Inherit some common Syberia stuff.
 $(call inherit-product, vendor/syberia/common.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/mainline.mk)
 
 #bootanimation
 TARGET_BOOT_ANIMATION_RES := 1080
